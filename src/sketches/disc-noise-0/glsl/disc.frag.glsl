b9b818e9700a9961e29737b370c2daa517cf0552
hamid814 <hamid331994@gmail.com>
2021-06-16 13:02:50 +0430

    new itteration - disc-noise

---
+++ src/sketches/disc-noise-0/glsl/disc.frag.glsl
@@ -0,0 +1,23 @@
+#pragma glslify: noise = require(../../../glsl-util/perlin/3d)
+
+uniform float ratio;
+uniform float uTime;
+
+varying vec2 vUv;
+
+void main() {
+  float time = uTime * 3.0;
+  
+  vec2 ratioUV = vUv;
+  ratioUV.x *= ratio;
+  float shiftRatio = ratio / 2.0 - 0.5;
+  ratioUV.x -= shiftRatio;
+
+  vec2 noiseUV = ratioUV * 4.0;
+
+  float noiseValue = noise(vec3(noiseUV.x, noiseUV.y, time)) - 0.0;
+
+  float strength = 1.0 - step(abs(sin(distance(ratioUV, vec2(0.5)) * 150.0) - noiseValue), 0.71);
+
+  gl_FragColor = vec4(strength / 1.5, strength / 1.5, strength / 1.5, 1.0);
+}
